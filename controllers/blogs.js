949ede8abe4d206300d5a57c83bf266982351874
Eetu <eskohv@utu.fi>
2020-12-06 22:20:54 +0200

    on 4.18, testing left

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,14 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog
+        .find({}).populate('user',{ username: 1, name: 1 })
+
+    response.json(blogs)
+
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -19,23 +21,42 @@ blogsRouter.get('/:id', async (request, response) => {
     }
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if  (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
-    const blog = await new Blog(request.body)
+    const blog = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+        return response.status(401).json( { error: 'token missing or invalid' })
+    }
+
     if(!blog.title || !blog.url){
         response.send(400)
     }
 
-    let newBlog =  {
+    const user = await User.findById(decodedToken.id)
+
+    const newBlog =  new Blog({
         title: blog.title,
         author: blog.author,
         url: blog.url,
-        likes: blog.likes || 0
-    }
-    newBlog = new Blog(newBlog)
+        likes: blog.likes || 0,
+        user: user._id
+    })
 
-    const result = await newBlog.save()
+    const savedBlog = await newBlog.save()
+    user.blogs = user.blogs.concat(savedBlog)
+    await user.save()
 
-    response.status(201).json(result)
+    response.json(savedBlog.toJSON())
 
 
 })
